80cabda8a0b9d96b90c6ac38e5aac9160de27b73
hyun <hyun996@gmail.com>
2021-11-25 14:28:27 +0900

    first commit

---
+++ spyder-test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 18 07:43:40 2021
+
+@author: hipbone
+"""
+
+print("hello")
+
+var = 'a'
+list = ['a','b']
+print(var, list)
